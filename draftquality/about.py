9a151c1a04068a862874a1ca205daa7e91a43717
halfak <aaron.halfaker@gmail.com>
2017-01-30 15:19:56 -0600

    Adds basic project stuff.

---
+++ draftquality/about.py
@@ -0,0 +1,8 @@
+__name__ = "draftquality"
+__version__ = "0.0.1"
+__author__ = "Aaron Halfaker"
+__author_email__ = "ahalfaker@wikimedia.org"
+__description__ = "A library for performing automatic detection of the " + \
+                  "quality of Wikipedia article creations."
+__url__ = "https://github.com/wiki-ai/draftquality"
+__license__ = "MIT"
