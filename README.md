f2ee9b8397454e3a30e04956dc94bf57be387661
Amir Sarabadani <ladsgroup@gmail.com>
2018-08-22 16:55:03 +0200

    Update link to the github repo

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This library provides a set of utilities for building and maintaining
 draft (new article creation) quality prediction models.
 
 * **Installation:** ``pip install draftquality``
-* **Repository:** https://github.com/wiki-ai/draftquality
+* **Repository:** https://github.com/wikimedia/draftquality
 * **License:** MIT
 
 ## Requirements
--- draftquality/about.py
+++ draftquality/about.py
@@ -4,5 +4,5 @@ __author__ = "Aaron Halfaker"
 __author_email__ = "ahalfaker@wikimedia.org"
 __description__ = "A library for performing automatic detection of the " + \
                   "quality of Wikipedia article creations."
-__url__ = "https://github.com/wiki-ai/draftquality"
+__url__ = "https://github.com/wikimedia/draftquality"
 __license__ = "MIT"
